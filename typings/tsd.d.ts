9747a74397bf47965d9427d300b459eb0437f6b3
ankursoni <me.soni88@gmail.com>
2016-03-01 11:54:32 +0530

    dependencies added

---
+++ typings/tsd.d.ts
@@ -0,0 +1,8 @@
+/// <reference path="angularjs/angular.d.ts" />
+/// <reference path="angular-ui-bootstrap/angular-ui-bootstrap.d.ts"/>
+/// <reference path="angularjs/angular-route.d.ts" />
+/// <reference path="requirejs/require.d.ts"/>
+/// <reference path="jquery/jquery.d.ts"/>
+/// <reference path="lodash/lodash.d.ts"/>
+/// <reference path="angular-ui-router/angular-ui-router.d.ts"/>
+/// <reference path="restangular/restangular.d.ts"/>
